c916d759a8d8218390f5d61d89d2ca1bd086e9be
novichikhin alexey <novichikhin.au@gmail.com>
2021-09-03 10:12:30 +0300

    upload

---
+++ template/src/Service.AssetsDictionary.Client/AssetsDictionaryClientFactory.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using MyJetWallet.Sdk.Grpc;
+using Service.AssetsDictionary.Grpc;
+
+namespace Service.AssetsDictionary.Client
+{
+    [UsedImplicitly]
+    public class AssetsDictionaryClientFactory: MyGrpcClientFactory
+    {
+        public AssetsDictionaryClientFactory(string grpcServiceUrl) : base(grpcServiceUrl)
+        {
+        }
+
+        public IHelloService GetHelloService() => CreateGrpcService<IHelloService>();
+    }
+}
